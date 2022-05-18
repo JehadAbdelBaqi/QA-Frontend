8352a8767508a61e4c2b6583a54ca420029675b0
Jehad <jehad.abdelbaqi@gmail.com>
2022-05-18 12:23:47 +0100

    JA: Added extra exercise to jsscript file at bottom of file.

--- JS/jsscript.js
+++ JS/jsscript.js
@@ -5,6 +5,12 @@ console.log("Taurus");
 
 favouriteCarMake = "Nissan";
 favouriteCarModel = "Skyline";
-console.log("%c My favourite car is a ${favouriteCarMake} and the model is ${favouriteCarModel}.");
+console.log(`My favourite car is a ${favouriteCarMake} and the model is ${favouriteCarModel}.`);
 
 console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
