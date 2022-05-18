9126c7b1b7c873eee38780b24d88db65b6a94ea2
Jehad <jehad.abdelbaqi@gmail.com>
2022-05-18 11:39:19 +0100

    JA: Commited first JS file and script exercise

---
+++ JS/jsscript.js
@@ -0,0 +1,10 @@
+console.log("Jehad");
+console.log("AbdelBaqi");
+console.log("London");
+console.log("Taurus");
+
+favouriteCarMake = "Nissan";
+favouriteCarModel = "Skyline";
+console.log("%c My favourite car is a ${favouriteCarMake} and the model is ${favouriteCarModel}.");
+
+console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
